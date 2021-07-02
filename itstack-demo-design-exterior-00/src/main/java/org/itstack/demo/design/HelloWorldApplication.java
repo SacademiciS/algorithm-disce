2556d4ee043a7239a31152fe25c48a9e9b6a964f
wujiahua <wujiahua@feinno.com>
2021-07-02 12:30:50 +0800

    2021年7月2日 12:27:08 外观模式提交

--- itstack-demo-design-exterior-00/src/main/java/org/itstack/demo/design/HelloWorldApplication.java
+++ itstack-demo-design-exterior-00/src/main/java/org/itstack/demo/design/HelloWorldApplication.java
@@ -1,14 +1,10 @@
 package org.itstack.demo.design;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-
-@SpringBootApplication
-@Configuration
-public class HelloWorldApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
-    }
-}
+//@SpringBootApplication
+//@Configuration
+//public class HelloWorldApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(HelloWorldApplication.class, args);
+//    }
+//}
